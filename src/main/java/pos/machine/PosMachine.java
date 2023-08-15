1c33a6914fb6e27e625a167f220b60e0b184245f
TACULMA2 <marigold.taculog@oocl.com>
2023-08-15 22:34:25 +0800

    feat: add printReceipt function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,18 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.Map;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        return null;
+        List<Item> items = ItemsLoader.loadAllItems();
+        Map<String, Integer> itemCounts = countItemOccurrences(barcodes);
+
+        StringBuilder receipt = new StringBuilder("***<store earning no money>Receipt***\n");
+        int total = generateReceipt(items, receipt, itemCounts);
+
+        appendTotal(receipt, total);
+        return receipt.toString();
     }
+
 }
